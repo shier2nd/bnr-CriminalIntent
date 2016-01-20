85bcbbedba8d810271cd998f87fc72e026920491
shier2nd <shier.2nd@gmail.com>
2016-01-20 12:36:02 +0800

    bug fixed

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -193,7 +193,7 @@ public class CrimeFragment extends Fragment {
             return;
         }
 
-        if (requestCode == REQUEST_DATE && requestCode == REQUEST_TIME) {
+        if (requestCode == REQUEST_DATE || requestCode == REQUEST_TIME) {
             Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mCrime.setDate(date);
 
